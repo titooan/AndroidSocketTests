7b0363982c6fb88e6777f7395679251a83597485
Titouan <titouan.thibaud@gmail.com>
2015-04-14 17:10:05 +0200

    Added Nsd. Client is now able to discover Server adress and port.

--- SocketTests/app/src/main/java/com/titouan/sockettests/ClientActivity.java
+++ SocketTests/app/src/main/java/com/titouan/sockettests/ClientActivity.java
@@ -3,6 +3,7 @@ package com.titouan.sockettests;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,11 +14,13 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.PublicKey;
 
 
 public class ClientActivity extends ActionBarActivity {
@@ -28,21 +31,25 @@ public class ClientActivity extends ActionBarActivity {
     private TextView tvMessage;
     private Handler updateConversationHandler;
 
-    private static final String SERVER_IP = "192.168.1.22";
-    public static final int SERVER_PORT = ServerActivity.SERVERPORT;
-
     private PrintWriter out = null;
     private BufferedReader in = null;
+
+    private NsdHelper mNsdHelper;
+
+    private Thread clientThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("State", "Create");
         setContentView(R.layout.activity_client);
 
         message = (EditText) findViewById(R.id.message);
         tvMessage = (TextView) findViewById(R.id.text);
 
-        new Thread(new ClientThread()).start();
+        mNsdHelper = new NsdHelper(this);
+        mNsdHelper.initializeDiscoveryListener();
+        mNsdHelper.discoverServices();
 
         updateConversationHandler = new Handler();
     }
@@ -50,6 +57,12 @@ public class ClientActivity extends ActionBarActivity {
     public void send(View v){
         out.println(message.getText().toString());
         out.flush();
+        message.setText("");
+    }
+
+    public void connectToServer(InetAddress hostAddress, int hostPort){
+        clientThread = new Thread(new ClientThread(hostAddress, hostPort));
+        clientThread.start();
     }
 
 
@@ -77,42 +90,50 @@ public class ClientActivity extends ActionBarActivity {
 
     class ClientThread implements Runnable {
 
+        private InetAddress hostAddress;
+        private int hostPort;
+
+        ClientThread(InetAddress hostAddress, int hostPort) {
+            this.hostAddress = hostAddress;
+            this.hostPort = hostPort;
+        }
+
         @Override
         public void run(){
-            tvMessage.setText("Try");
             try{
-                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-                socket = new Socket(serverAddr, SERVER_PORT);
+                socket = new Socket(hostAddress, hostPort);
+
+                mNsdHelper.stopDiscovery();
+
                 updateConversationHandler.post(new UpdateUIThread("Connected."));
+
+                out = new PrintWriter(socket.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                while(!Thread.currentThread().isInterrupted()){
+                    try{
+                        String read = in.readLine();
+                        if(read == null){
+                            updateConversationHandler.post(new UpdateUIThread("Connexion closed."));
+                            out.close();
+                            in.close();
+                            socket.close();
+                            return;
+                        }
+
+                        updateConversationHandler.post(new UpdateUIThread(read));
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
             }catch(UnknownHostException e1){
                 e1.printStackTrace();
             }catch(IOException e2){
                 e2.printStackTrace();
-            }
-
-            try {
-                out = new PrintWriter(socket.getOutputStream());
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            } catch (Exception e) {
+            }catch (Exception e) {
                 e.printStackTrace();
             }
 
-            while(!Thread.currentThread().isInterrupted()){
-                try{
-                    String read = in.readLine();
-                    if(read == null){
-                        updateConversationHandler.post(new UpdateUIThread("Connexion closed."));
-                        out.close();
-                        in.close();
-                        socket.close();
-                        return;
-                    }
-
-                    updateConversationHandler.post(new UpdateUIThread(read));
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
@@ -125,7 +146,38 @@ public class ClientActivity extends ActionBarActivity {
 
         @Override
         public void run(){
-            tvMessage.setText(tvMessage.getText().toString()+msg+"\n");
+            display(this.msg);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e("State", "Pause");
+        if (mNsdHelper != null) {
+            mNsdHelper.stopDiscovery();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e("State", "Resume");
+        super.onResume();
+    }
+
+    private void display(String str){
+        tvMessage.setText(str+"\n"+tvMessage.getText().toString());
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e("State", "Destroy");
+        super.onDestroy();
+        try {
+            clientThread.interrupt();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- SocketTests/app/src/main/java/com/titouan/sockettests/ServerActivity.java
+++ SocketTests/app/src/main/java/com/titouan/sockettests/ServerActivity.java
@@ -1,5 +1,8 @@
 package com.titouan.sockettests;
 
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
@@ -15,14 +18,19 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.crypto.Cipher;
 
 
 public class ServerActivity extends ActionBarActivity {
@@ -31,11 +39,18 @@ public class ServerActivity extends ActionBarActivity {
     private Handler updateConversationHandler;
     private Thread serverThread = null;
     private List<CommunicationThread> clientThreads;
+    private int localPort = -1;
 
     private TextView text;
     private EditText message;
 
-    public static final int SERVERPORT = 6000;
+    private NsdHelper mNsdHelper;
+
+    //Encryption constants
+    private static final String RSA_ALGORITHM = "RSA";
+    private static final String AES_ALGORITHM = "AES/CBC/PKCS7Padding";
+    private static final String PUBLIC_KEY_FILE = "public.key";
+    private static final String PRIVATE_KEY_FILE = "private.key";
 
 
     @Override
@@ -49,7 +64,7 @@ public class ServerActivity extends ActionBarActivity {
 
         WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
         String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
-        text.setText(ip);
+        text.setText(ip+"\n");
 
         clientThreads = new ArrayList<>();
 
@@ -57,16 +72,48 @@ public class ServerActivity extends ActionBarActivity {
 
         this.serverThread = new Thread(new ServerThread());
         this.serverThread.start();
+
+        /*
+        // Encryption tests
+        try {
+            //Getting RSA PublicKey from file
+            ObjectInputStream inputStream = new ObjectInputStream(getAssets().open(PUBLIC_KEY_FILE));
+            final PublicKey publicKey = (PublicKey) inputStream.readObject();
+            inputStream = new ObjectInputStream(getAssets().open(PRIVATE_KEY_FILE));
+            final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
+
+            final String originalText = "Text to be encrypted ";
+            final byte[] cipherText = encrypt(originalText, publicKey);
+            final String plainText = decrypt(cipherText, privateKey);
+
+            String encrypted = new String(rsaEncrypt("Bonjour".getBytes(), publicKey));
+            display(new String(cipherText));
+            display(plainText);
+        }catch(Exception e){
+            display("Encryption failed.");
+            e.printStackTrace();
+        }*/
+    }
+
+    private void display(String str){
+        text.setText(str+"\n"+text.getText().toString());
     }
 
     class ServerThread implements Runnable{
         public void run(){
             Socket socket = null;
             try{
-                serverSocket = new ServerSocket(SERVERPORT);
+                serverSocket = new ServerSocket(0);
+                localPort = serverSocket.getLocalPort();
             }catch (IOException e){
                 e.printStackTrace();
             }
+
+            mNsdHelper = new NsdHelper(ServerActivity.this);
+            mNsdHelper.initializeRegistrationListener();
+            mNsdHelper.registerService(localPort);
+
+            updateConversationHandler.post(new UpdateUIThread("Port : "+localPort));
 
             while(!Thread.currentThread().isInterrupted()){
                 try{
@@ -190,5 +237,89 @@ public class ServerActivity extends ActionBarActivity {
             e.printStackTrace();
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mNsdHelper.tearDown();
+        try{
+            serverSocket.close();
+            serverThread.interrupt();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Encrypt the text using public key
+     * @param text original text/key/iv
+     * @return
+     */
+    private byte[] rsaEncrypt(byte[] text, PublicKey key) {
+        byte[] cipherText = null;
+        try {
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            // encrypt the plain text using the public key
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            cipherText = cipher.doFinal(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cipherText;
+    }
+
+    /**
+     * Encrypt the plain text using public key.
+     *
+     * @param text
+     *          : original plain text
+     * @param key
+     *          :The public key
+     * @return Encrypted text
+     * @throws java.lang.Exception
+     */
+    public static byte[] encrypt(String text, PublicKey key) {
+        byte[] cipherText = null;
+        try {
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            // encrypt the plain text using the public key
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            cipherText = cipher.doFinal(text.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cipherText;
+    }
+
+    /**
+     * Decrypt text using private key.
+     *
+     * @param text
+     *          :encrypted text
+     * @param key
+     *          :The private key
+     * @return plain text
+     * @throws java.lang.Exception
+     */
+    public static String decrypt(byte[] text, PrivateKey key) {
+        byte[] dectyptedText = null;
+        try {
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+
+            // decrypt the text using the private key
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            dectyptedText = cipher.doFinal(text);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return new String(dectyptedText);
+    }
+
 
 }
